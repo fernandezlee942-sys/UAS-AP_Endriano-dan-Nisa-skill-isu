bbdd1e44af16cfc93d579f015867bb19ea8773d2
Fernandez <fernandezlee942@gmail.com>
2026-06-07 16:52:42 +0700

    deleted mthe whole comment

--- MineSweeper.cpp
+++ MineSweeper.cpp
@@ -1,24 +1,3 @@
-// idk why but midcode i think the whole input shit broke, i kinda inputted 100000000 and the other input is somehow affected --> but when i try again somehow the bug didnt appear again praise the lord and his name and maybe myself form somehow eradicatinig bugs tht i didnt even understand come from where
-
-
-
-
-// goddamn i've just realized how bad this code is, its kinda a mmircale it works especially the cin for the koordinat tx and ty (i;ve just realizsed abt it but, i think i typoed the tx --> should have been just x and just goes on with it for the whole entire code goddamn i wanna save this version as a back up to prove im worse than yandere dev)
-
-
-
-// (currently the input method use in this whole program only stops user from typing random bullshit like string in the input --> you can still input float like 1.2 without any problems but try doing it with smtg like 1.232345342, the program itself still works normally but you'll trigger the safety mechanism added to the next cin so embrace for the impact, as for why I didnt make it so user cant input float, I'm too deep into this shit, wished I noticed it much earlier. If you input smtg like 1.2332342 I assume that its not likely ur trying to play the game normally but just trying to find a bug or 2 --> dev note: sorry for the impoliteness not quite used to polite words in english)
-
-
-
-//i use label when im too lazy to think whether the right syntax is continue or break dont judge me its just tht much easier
-
-// i've realized it midway but i could've just make the clearinputbuffer into a function that took string parameter(the error name) but apparently im too lazy to change it now
-
-
-//gosh i wished i read that starting index=1 requirement before the start of this project
-
-
 #define Fernandez_Lebih_Ganteng_Dari_Justin_Bieber true
 #include <iostream>
 #include <cstdlib>
@@ -41,8 +20,8 @@ public:
     int bombAround[10][10];
     bool isUnlocked[10][10];
     bool isFlagged[10][10];
-    bool gameResult=true;  //true for win false for lose
-    bool isFilled[10][10]; //--> would like to name it isOccupied but tbh isFilled sounds better
+    bool gameResult=true;
+    bool isFilled[10][10];
 
     
     bool AllFilled(){
@@ -93,7 +72,6 @@ public:
     void play(){
         startTime=time(0);
         gameResult=true;
-        //map reset
         for(int j = 0; j < mapSize; j++) {
             for(int i = 0; i < mapSize; i++) {
                 isBomb[i][j] = false;
@@ -136,7 +114,6 @@ public:
                 
                 goto g;
             }
-            // again, i know how much of a bad practice it is but tbh the amount of line this code has is enough to stop me from touching this spaghetti code
 
             if(AllFilled()==true){
                 cout<<"\n 4. Check answer (no return after this process)";
@@ -185,9 +162,7 @@ public:
                             cout<<"Coordinates has already been flagged unlock it before flagging it\n";
                             system("pause");
                         }
-                        // no need to complain abt how bad of a practice this is i know it myself, should've make a bgi if's before all of this to check for isflagged, since i did all of this wihtout planning shit i think it's kinda natural when i add new feature some legacy code needs a quick fix and this's one of the easiest i can do
                         else if((koordinatTY>=0)&&(koordinatTY<mapSize)){
-                            // isUnlocked[koordinatTX][koordinatTY]=true;
                             BlankspaceHandler(koordinatTX,koordinatTY);
                         }
 
@@ -276,8 +251,6 @@ public:
                 cout<<"Input Map Size (map should be bigger than 3 but less than 10,for 9*9 --> just type 9) : "; 
                 cin>>mapSize;
 
-                //small note, jlh bomb hrs lebih kecil dari ukuran map --> jaga2 kelupaan, persentase bom harus dibawah 20% ukuran map
-
                 if((mapSize<=10)&&(mapSize>=3)){
                     c:
                     cout<<"Input Bomb Total (the amount of bomb should be less or equal to 20% of the map size) : ";
@@ -290,7 +263,6 @@ public:
                     else{
                         ClearInputBuffer();
                         cout<<"Invalid Total Bomb, choose an  integer below the 20% of the map size\n";
-                        //goddamn i fkin hate the % from c
                         system("pause");
                         goto c;
                     }                    
@@ -306,7 +278,6 @@ public:
             else if (menu==2){
                 cout<<"Thanks for playing!";
                 exit(0);
-                //you know how i kept using goto label whenver im too lazy to think abt anythuing? ye tht turned into a problem apparently the goto g; make a whole new session and break didnt quite get me out of the whole while loop so after some discussion with my buddy gpt o've choosed to use exit to just terminate the whole shit
 
             }
             else{
@@ -350,16 +321,14 @@ public:
         cout<<"Play Time (sadly I'm unable to make this timer go live since we have cin to run --> we need other libraries): "<<playTime<<endl;
         
         for(int j = mapSize-1;j>=0;j--){
-            if(j==9){ //biar row 10 g kelihatan lucu
+            if(j==9){ 
                 cout<<"Row "<<j+1<<"  ";
             }
 
-            else if (j<=mapSize) //b4 someone ask why the hell <= mapsize, i would like to defend myslf by saying that i mistaken the logic since i play test the whole thing with 10*10, so the old code is like, if j==mapsize-1, add one space more in row, so now i changed it into when row == 10 add one more space and for other situation make it less one space bar, since we checked the j==10 first it fixed the shit
+            else if (j<=mapSize)
             {
                 cout<<"Row "<<j+1<<"   ";
             }
-            
-            //ini cmn arraynya aja jadi g kt sentuh walau agak g enak buat dibaca array [0][0] buat koordinat row 1 col 1
             for(int i=0;i<mapSize;i++){
 
                 if (isUnlocked[i][j]==true){
@@ -413,15 +382,11 @@ public:
     void checkSurrounding() {
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
-                if (isBomb[i][j]) continue; // Skip if this tile itself is a bomb
-
-                // Look at all 8 directions around coordinate (i, j)
+                if (isBomb[i][j]) continue;
                 for (int xOffset = -1; xOffset <= 1; xOffset++) {
                     for (int yOffset = -1; yOffset <= 1; yOffset++) {
                         int ni = i + xOffset;
                         int nj = j + yOffset;
-
-                        // Ensure neighbor is inside the valid map bounds
                         if ((ni >= 0)&&(ni < mapSize)&&(nj >= 0)&&(nj < mapSize)) {
                             if (isBomb[ni][nj]==true) {
                                 bombAround[i][j]++;
@@ -438,14 +403,12 @@ public:
     }
 
     void BlankspaceHandler(int x, int y) {
-        // Base cases: stop if out of bounds, already unlocked, or flagged
         if (x < 0 || x >= mapSize || y < 0 || y >= mapSize) return;
+
         if (isUnlocked[x][y] || isFlagged[x][y]) return;
 
-        // Reveal the current tile
         isUnlocked[x][y] = true;
 
-        // If it's a blank tile (0 bombs around), recursively call on all 8 neighbors
         if (bombAround[x][y] == 0) {
             for (int xOffset = -1; xOffset <= 1; xOffset++) {
                 for (int yOffset = -1; yOffset <= 1; yOffset++) {
@@ -459,11 +422,9 @@ public:
 
 
 int main() {
-    srand(time(0)); //this kid from ctime used to make sure the randomnumbergenerator result changes each session
+    srand(time(0));
     system("cls");
     minesweeper inigamefrfr;
     inigamefrfr.playMenu();
     return 0;
 }
-
-// scrapped the whole (first guess will always be a empty space, since it is not in the requirements)
